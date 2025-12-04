e77d15a3e5f3d0907f9f8dc2517d362185ac7fe0
hoseajavier <112994063+hoseajavier@users.noreply.github.com>
2025-12-04 14:16:08 +0700

    adding detail gambar chino

--- src/data/featuredProduct.js
+++ src/data/featuredProduct.js
@@ -2,6 +2,9 @@ import chino1 from "../assets/product/chino_front.jpg";
 import chino2 from "../assets/product/chino_front1.jpg";
 import chino3 from "../assets/product/chino_diag.jpg";
 import chino4 from "../assets/product/chino_back.jpg";
+import chino5 from "../assets/product/detail-chino-front.jpg";
+import chino6 from "../assets/product/detail-chino-chino_back.jpg";
+import chino7 from "../assets/product/detail-chino-diag.jpg";
 
 const featuredProduct = {
   id: 1,
@@ -9,7 +12,7 @@ const featuredProduct = {
   price: "Rp99.900",
   description:
     "Comfortable premium chino pants made from durable and breathable materials, suitable for daily wear.",
-  images: [chino1, chino2, chino3, chino4],
+  images: [chino1, chino2,chino5, chino3, chino6, chino4, chino7],
   stock: {
     M: 7,
     L: 17,
